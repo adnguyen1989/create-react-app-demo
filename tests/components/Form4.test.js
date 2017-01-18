d4cb24320711c748303d828dde3852464597cc07
Anh Nguyen <adnguyen1989@gmail.com>
2017-01-18 15:52:54 +0700

    add testing

---
+++ tests/components/Form4.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+import Form4 from '../../src/components/Form4'
+
+const wrapper = shallow(<Form4 />);
+
+describe('(Component) Form4', () => {
+  it('renders...', () => {
+    expect(wrapper).to.have.length(1);
+  });
+});
+
