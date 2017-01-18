afee638e8e610619430dd1d4e8b92d3f5b076fc3
Anh Nguyen <adnguyen1989@gmail.com>
2017-01-18 18:16:28 +0700

    write some sample tests

--- tests/components/Form4.test.js
+++ tests/components/Form4.test.js
@@ -2,12 +2,69 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import Form4 from '~/src/components/Form4'
+import sinon from 'sinon';
 
-const wrapper = shallow(<Form4 />);
 
 describe('(Component) Form4', () => {
-  it('renders...', () => {
-    expect(wrapper).to.have.length(1);
-  });
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Form4 />)
+  })
+
+  describe('renders static elements', () => {
+    it('renders the title', () => {
+      expect(wrapper.containsMatchingElement(<h1>Sign Up Sheet</h1>)).to.equal(true);
+    });
+
+    it('renders the list', () => {
+      expect(wrapper.containsMatchingElement(<h3>People</h3>)).to.equal(true);
+    });
+
+    it('renders two fields', () => {
+      expect(wrapper.find('form')).to.have.length(1);
+    });
+
+    it('renders two fields', () => {
+      expect(wrapper.find('Field')).to.have.length(2);
+    });
+
+    it('renders one course select', () => {
+      expect(wrapper.find('CourseSelect')).to.have.length(1);
+    });
+
+    it('renders one submit button', () => {
+      expect(wrapper.containsMatchingElement(<input type='submit'></input>)).to.equal(true);
+    });
+  })
+
+  describe('renders the state of submit button', ()=>{
+    it ('renders the ready state',()=>{
+      wrapper.setState({_saveStatus: "READY"});
+      const input = wrapper.find('input').first();
+      expect(input.props().value).to.equal("Submit");
+    });
+
+    it ('renders the saving state',()=>{
+      wrapper.setState({_saveStatus: "SAVING"});
+      const input = wrapper.find('input').first();
+      expect(input.props().value).to.equal("Saving");
+    });
+
+    it ('renders the success state',()=>{
+      wrapper.setState({_saveStatus: "SUCCESS"});
+      const input = wrapper.find('input').first();
+      expect(input.props().value).to.equal("Success");
+    });
+
+    it ('renders the error state',()=>{
+      wrapper.setState({_saveStatus: "ERROR"});
+      const input = wrapper.find('input').first();
+      expect(input.props().value).to.equal("Save failed");
+    });
+
+  })
+
+
 });
 
